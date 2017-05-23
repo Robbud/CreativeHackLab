bb6a771d1d5471146d9235e7cf990cd4f3f92497
Robbud <budigins@gmail.com>
2017-05-23 18:24:36 +0100

    Update NEWS_API_TEST.py
    print lines added

--- NEWS_API_TEST.py
+++ NEWS_API_TEST.py
@@ -6,14 +6,15 @@
 import requests, json
 
 # put the apikey you got when you subscribed below:
-apikey = "your apikey"
+apikey = "YOUR API KEY "
 
 # change this to the one you want or
 # maybe have an array of sources that you loop through?
-source = "techcrunch"
+source = "EXAMPLESOURCE"
 
 # the url for the news api articles combining the  variables you defined above
-#url = 'https://newsapi.org/v1/articles?source= source here&apiKey= api key here '
+url = 'https://newsapi.org/v1/articles?source= SOURCE &apiKey= YOUR API KEY '
+
 
 
 
@@ -22,11 +23,16 @@ r = requests.get(url)
 # load the response as json formated data
 news = json.loads(r.content)
 
+
 # this will list all the available keys
 print(news["articles"][0].keys())
-# this outputs [u'description', u'title', u'url', u'author', u'publishedAt', u'urlToImage']
+#print[u'description', u'title', u'url', u'author', u'publishedAt', u'urlToImage']
 
 # now i will use a "for loop" to cycle through each of the articles
 # and print the "title' key followed by a line break (\n  newline)
 for item in news['articles']:
     print(item["title"] + "\n")
+    print(item["author"] + "\n")
+    print(item["url"] + "\n") #prints link to news
+    print(item["description"] + "\n") #shows description
+    print(item["publishedAt"] + "\n") #shows the publication time
