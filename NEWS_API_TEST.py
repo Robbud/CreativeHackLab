f718e03ad5d4df2476b8ad7bcca99b8b94169f94
Robbud <budigins@gmail.com>
2017-05-25 10:42:02 +0000

    Update NEWS_API
    added urllib

--- NEWS_API_TEST.py
+++ NEWS_API_TEST.py
@@ -4,7 +4,8 @@
 # maybe you need to install something extra to get these?
 # sudo pip install requests json maybe?
 import requests, json
-
+import os
+import urllib
 # put the apikey you got when you subscribed below:
 apikey = "YOUR API KEY "
 
@@ -13,15 +14,22 @@ apikey = "YOUR API KEY "
 source = "EXAMPLESOURCE"
 
 # the url for the news api articles combining the  variables you defined above
-url = 'https://newsapi.org/v1/articles?source= SOURCE &apiKey= YOUR API KEY '
+url = 'https://newsapi.org/v1/articles?source= YOUR SOURCE &apiKey= YOUR API KEY'
 
 
-
+image = urllib.URLopener()
 
 # make the get request from the url
 r = requests.get(url)
 # load the response as json formated data
 news = json.loads(r.content)
+
+
+# download images from url and save into Images folder.
+
+fullfilename = os.path.join('/home/pi/CreativeHackLab/Images','filename')
+urllib.urlretrieve("https://newsapi.org/v1/articles?source=espn&apiKey=YOUR API KEY","Images/Image.jpg")
+
 
 
 # this will list all the available keys
@@ -32,7 +40,11 @@ print(news["articles"][0].keys())
 # and print the "title' key followed by a line break (\n  newline)
 for item in news['articles']:
     print(item["title"] + "\n")
-    print(item["author"] + "\n")
-    print(item["url"] + "\n") #prints link to news
-    print(item["description"] + "\n") #shows description
-    print(item["publishedAt"] + "\n") #shows the publication time
+    #print(item["author"] + "\n")
+    print(item["urlToImage"] + "\n")
+    image.retrieve(item["urlToImage"])
+
+    #print(item["url"] + "\n") #prints link to news
+    #print(item["description"] + "\n") #shows description
+    #print(item["publishedAt"] + "\n") #shows the publication time
+
